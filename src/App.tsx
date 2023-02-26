e22ff7cde42768de692bca2cf516d0901e91c873
stesevicuros <stesevicu@gmail.com>
2023-02-26 22:01:46 +0100

    Napravio sam slucajni bag kad sam uklanjao merge-conflicts jednu zagradu vise sam obrisao pa cu da commitujem bez PRa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-
 import HomePage from "./styles/components/home-page/HomePage";
 import OnboardingPage from "./components/onboarding-flow/onboarding-page/OnboardingPage";
 
 function App() {
-	return 
-			<HomePage />
-
+	return <HomePage />;
 }
 
 export default App;
