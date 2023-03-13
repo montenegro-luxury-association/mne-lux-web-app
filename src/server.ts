1a359e86b08871cca99b43d06256822de6da3b51
g-popovic <georgepopovich14@gmail.com>
2023-03-13 20:10:36 +0100

    Create working 'Hello World' server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/", (_: Request, res: Response): void => {
+	res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+	console.log("Server started on port 3000");
+});
