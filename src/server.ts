2644fd0bdb2dc51edf7330dc408bd128447b2a9e
g-popovic <georgepopovich14@gmail.com>
2023-03-13 20:12:50 +0100

    Add dynamic port

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app: Application = express();
+const PORT = process.env.PORT || 3001;
 
 app.get("/", (_: Request, res: Response): void => {
 	res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-	console.log("Server started on port 3000");
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
 });
