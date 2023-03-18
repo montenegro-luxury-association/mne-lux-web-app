6cce863c087b8726296345069b9878c9039424d8
Marko Scekic <81862323+sceka@users.noreply.github.com>
2023-03-18 01:04:05 +0100

    Remove console.log

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -6,8 +6,6 @@ const router = Router();
 router.post("/:id", async (req: Request, res: Response) => {
 	try {
 		const newHotel = new Listing(req.body);
-		console.log(req.body);
-
 		await newHotel.save().then((item: any) => {
 			res.send("Item is saved in Database");
 		});
