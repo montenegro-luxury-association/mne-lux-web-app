373e3520d5c03d39dec1a64e7b3a3e81300de415
Marko Scekic <81862323+sceka@users.noreply.github.com>
2023-04-14 14:04:21 +0200

    Improve listing creation

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,16 +1,16 @@
 import { Request, Response, Router } from "express";
 import { Listing } from "../models/listing.model";
+import { log } from "console";
 
 const router = Router();
 
 router.post("/add-listing", async (req, res) => {
 	try {
 		const newListing = new Listing(req.body);
-		await newListing.save().then((item: any) => {
-			res.send("Item is saved in Database");
-		});
-	} catch (err: any) {
-		res.status(500).send();
+		await newListing.save();
+		res.json({ newListing: newListing.id });
+	} catch (err) {
+		res.sendStatus(500);
 	}
 });
 
