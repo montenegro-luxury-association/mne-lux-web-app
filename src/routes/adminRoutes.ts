dc05fe9440555f753d7e1b8c20d2bacbcb6a5221
g-popovic <georgepopovich14@gmail.com>
2023-04-14 13:50:30 +0200

    Remove any

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -6,10 +6,10 @@ const router = Router();
 router.post("/add-listing", async (req, res) => {
 	try {
 		const newListing = new Listing(req.body);
-		await newListing.save().then((item: any) => {
+		await newListing.save().then(() => {
 			res.send("Item is saved in Database");
 		});
-	} catch (err: any) {
+	} catch (err) {
 		res.status(500).send();
 	}
 });
